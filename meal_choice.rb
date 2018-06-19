610d79a3bfa3cca6384414394f4e3f082839d327
umxe <u_habiba@rocketmail.com>
2018-06-19 14:40:10 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -11,20 +11,16 @@ def lunch(l= "grilled cheese")
   puts "Noon is the best time for #{l}!"
 end
 def dinner(d = "salmon")
-  
-  
-
-
-
-
-
-
+  puts "Night time is the best time for #{d}!"
+end
 # Call the methods with puts and your own arguments here. Like this:
 puts snacks("Swedish fish")
 ## This returns "Any time, is the right time for Sweedish fish!"
 
 # call your methods here
-
+breakfast()
+lunch()
+dinner()
 
 # Call your methods without any arguments here
 puts snacks
